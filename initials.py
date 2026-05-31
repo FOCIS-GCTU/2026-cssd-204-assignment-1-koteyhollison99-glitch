2e30fb6c6831ca12ae091f04f995dcb8b7e250c9
koteyhollison99-glitch <koteyhollison99@gmail.com>
2026-05-31 19:01:58 +0000

    implement initials printing functionality in initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Kotey Christian
+# STUDENT ID:  2425402318
+# Email: koteyhollison99@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Kotey Christian, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #Print out my initials in Large styled size
+    print()
+    print("...KC")
+    print()
 
 
+
+    print()
+
+main()
+print("...KK.......kKK..............CCCCCC.....")
+print("...KK.....KK..............CCC...........")
+print("...KK....KK..............CC.............")
+print("...KK...KK.............CC...............")
+print("...KK.KK...............CC...............")
+print("...KK.KK...............CC...............")
+print("...KK...KK.............CC...............")
+print("...KK....KK..............CC.............")
+print("...KK.....KK......**......CC..........**")
+print("...KK.......KKK...**.........CCCCCC...**")
+
+print()
 
 main()
